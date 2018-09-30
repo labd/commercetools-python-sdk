628b3c49ddc1c50d5eaf553f34b36d50871ab344
Michael van Tellingen <michael@mvantellingen.nl>
2018-09-30 09:44:35 +0200

    Remove None values in the service input schemas (delete and query)
    We have no special MISSING value currently, but this seems to work fine
    for now.

--- src/commercetools/abstract.py
+++ src/commercetools/abstract.py
@@ -1,13 +1,30 @@
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, post_dump
 
 
-class AbstractDeleteSchema(Schema):
+class RemoveEmptyValuesMixin:
+    @post_dump
+    def remove_empty_values(self, data):
+        """Remove the key, value pairs for which the value is None.continue
+
+        This doesn't work if allow_none is set. And in the future we might also
+        want to remove values which are already the default to minimise the
+        params.
+
+        """
+        result = {}
+        for k, v in data.items():
+            if v is not None:
+                result[k] = v
+        return result
+
+
+class AbstractDeleteSchema(Schema, RemoveEmptyValuesMixin):
     version = fields.Integer()
 
 
-class AbstractQuerySchema(Schema):
-    where = fields.List(fields.String(required=False))
-    sort = fields.List(fields.String(required=False))
-    expand = fields.List(fields.String(required=False))
+class AbstractQuerySchema(Schema, RemoveEmptyValuesMixin):
+    where = fields.List(fields.String())
+    sort = fields.List(fields.String())
+    expand = fields.List(fields.String())
     limit = fields.Int(required=False)
     offset = fields.Int(required=False)

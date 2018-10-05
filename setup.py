dcba627309e96de591dbc3e62b6e0b1f8fcc43e8
Mike de Bock <mike.debock@masterwebber.nl>
2018-10-05 09:25:42 +0200

    version bump 0.2.0

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ with open("README.md") as fh:
 
 setup(
     name="commercetools-python-sdk",
-    version="0.1.0",
+    version="0.2.0",
     description="SDK for Commercetools",
     long_description=long_description,
     author="Lab Digital B.V.",

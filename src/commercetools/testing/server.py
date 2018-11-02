0604873bb8f0b1aaee56b6b13c1cbbe0a223b3e0
Michael van Tellingen <michael@mvantellingen.nl>
2018-11-02 11:54:17 +0100

    Fix mypy warnings

--- src/commercetools/testing/server.py
+++ src/commercetools/testing/server.py
@@ -1,4 +1,5 @@
 import logging
+import typing
 from wsgiref.simple_server import make_server
 
 import requests
@@ -14,16 +15,17 @@ logger = logging.getLogger(__name__)
 
 class HttpAdapter:
     def __init__(self):
-        self._matchers = []
+        self._matchers: typing.Any = []
 
     def add_matcher(self, matcher) -> None:
         self._matchers.append(matcher)
 
-    def match(self, request: _RequestObjectProxy) -> HTTPResponse:
+    def match(self, request: _RequestObjectProxy) -> typing.Optional[HTTPResponse]:
         for matcher in self._matchers:
             response = matcher(request)
             if response:
                 return response
+        return None
 
 
 class Server:
@@ -43,7 +45,7 @@ class Server:
         response = self._create_response(response)
         return response(environ, start_response)
 
-    def _create_request(self, environ: dict) -> requests.PreparedRequest:
+    def _create_request(self, environ: dict) -> requests.Request:
         req = webob.Request(environ)
         req.make_body_seekable()
         return requests.Request(

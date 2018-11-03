labd/commercetools-python-sdk

commit d368013ddc6c350d078096d0090f3f54f10ac31e

Bump to 1.1.0

setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 install_requires = [
     "requests>=2.7.0",
     "pytz",
-    "requests-oauthlib>=1.0.0",
+    "requests-oauthlib>=1.1.0",
     "attrs>=18.2.0",
     "marshmallow>=3.0.0b17",
     "marshmallow-enum==1.4.1",
@@ -40,7 +40,7 @@ with open("README.rst") as fh:
 
 setup(
     name="commercetools",
-    version="1.0.0",
+    version="1.1.0",
     description="SDK for Commercetools",
     long_description=long_description,
     author="Lab Digital B.V.",

setup.cfg
@@ -1,5 +1,5 @@
 [bumpversion]
-current_version = 1.0.0
+current_version = 1.1.0
 
 [flake8]
 max-line-length = 99

commit 78a88b55f3f0ba3c2a465f8f5fa3221821bcfcc7

fix requests-oauthlib error introduced in d368013

setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 install_requires = [
     "requests>=2.7.0",
     "pytz",
-    "requests-oauthlib>=1.1.0",
+    "requests-oauthlib>=1.0.0",
     "attrs>=18.2.0",
     "marshmallow>=3.0.0b17",
     "marshmallow-enum==1.4.1",
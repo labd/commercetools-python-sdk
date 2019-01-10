f209d89e0f94e43dea3c0fe91c21487f25aea1bb
Michael van Tellingen <michael@mvantellingen.nl>
2019-01-10 13:27:54 +0100

    Properly convert ShippingMethodDraft to ShippingMethod
    The ZoneRate's where not converted from draft to the real objects

--- src/commercetools/testing/shipping_methods.py
+++ src/commercetools/testing/shipping_methods.py
@@ -1,3 +1,4 @@
+import copy
 import datetime
 import typing
 import uuid
@@ -13,6 +14,23 @@ class ShippingMethodsModel(BaseModel):
         self, obj: types.ShippingMethodDraft, id: typing.Optional[str] = None
     ) -> types.ShippingMethod:
         object_id = str(uuid.UUID(id) if id is not None else uuid.uuid4())
+
+        zone_rates: typing.List[types.ZoneRate] = []
+        for rate_draft in obj.zone_rates or []:
+            shipping_rates: typing.List[types.ShippingRate] = []
+            for shipping_rate_draft in rate_draft.shipping_rates or []:
+                shipping_rate = types.ShippingRate(
+                    price=shipping_rate_draft.price,
+                    free_above=shipping_rate_draft.free_above,
+                    tiers=copy.deepcopy(shipping_rate_draft.tiers),
+                )
+                shipping_rates.append(shipping_rate)
+
+            zone_rate = types.ZoneRate(
+                zone=rate_draft.zone, shipping_rates=shipping_rates
+            )
+            zone_rates.append(zone_rate)
+
         return types.ShippingMethod(
             id=str(object_id),
             key=obj.key,
@@ -22,7 +40,7 @@ class ShippingMethodsModel(BaseModel):
             name=obj.name,
             description=obj.description,
             tax_category=obj.tax_category,
-            zone_rates=obj.zone_rates,
+            zone_rates=zone_rates,
             is_default=obj.is_default,
             predicate=obj.predicate,
         )

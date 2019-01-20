4281c82a1611049fb71b7a284209f9ba1f4b4260
Michael van Tellingen <michael@mvantellingen.nl>
2019-01-20 18:19:10 +0100

    Add QueryPredicate() which can generate predicate strings
    Instead of constructing query strings yourself you can now use the
    QueryPredicate to build the string. It's modelled after Django's Q object.
    
    Examples:
    	`QueryPredicate(masterVariant__slug__en-=X)`
    	`QueryPredicate(**{"masterVariant__slug__en-GB": X})`

---
+++ src/commercetools/predicates.py
@@ -0,0 +1,77 @@
+import json
+from collections.abc import Collection
+from decimal import Decimal
+from functools import reduce
+
+
+class QueryPredicate:
+    AND = "AND"
+    OR = "OR"
+
+    _operators = {
+        "exact": "=",
+        "gte": ">=",
+        "lte": "=<",
+        "lt": "<",
+        "gt": ">",
+        "is_defined": ("is defined", "is not defined"),
+        "contains_all": "contains all",
+        "contains_any": "contains any",
+    }
+
+    def __init__(self, **filters: str):
+        self._connector = filters.pop("_connector", self.AND)
+        self._filters = filters
+
+    def __str__(self):
+        result = []
+
+        for key, value in self._filters.items():
+            fields = key.split("__")
+            operator = fields.pop()
+            if operator not in self._operators:
+                fields.append(operator)
+                operator = "exact"
+
+            lhs = fields.pop()
+            val = self._clause(lhs, operator, value)
+            fields.append(val)
+            result.append(reduce(lambda x, y: f"{y}({x})", fields[::-1]))
+
+        if self._connector == self.OR:
+            return " OR ".join(result)
+        return " AND ".join(result)
+
+    def __or__(self, other):
+        data = {}
+        data.update(self._filters)
+        data.update(other._filters)
+        return self.__class__(**data, _connector=self.OR)
+
+    def __and__(self, other):
+        data = {}
+        data.update(self._filters)
+        data.update(other._filters)
+        return self.__class__(**data, _connector=self.AND)
+
+    def _clause(self, lhs, operator, rhs):
+        assert operator in self._operators
+
+        if isinstance(rhs, self.__class__):
+            return "%s(%s)" % (lhs, rhs)
+
+        op = self._operators[operator]
+        if isinstance(op, tuple):
+            return " ".join((lhs, op[0 if rhs else 1]))
+        else:
+            rhs = self._escape_value(rhs)
+            return " ".join((lhs, op, rhs))
+
+    def _escape_value(self, value):
+        if isinstance(value, self.__class__):
+            return "(%s)" % value
+        if isinstance(value, Decimal):
+            return str(value)
+        if not isinstance(value, str) and isinstance(value, Collection):
+            return "(%s)" % (", ".join(self._escape_value(v) for v in value))
+        return json.dumps(value)
---
+++ tests/test_predicate.py
@@ -0,0 +1,61 @@
+from commercetools.predicates import QueryPredicate
+
+
+def test_simple():
+    obj = QueryPredicate(masterVariant__prices__country="NL")
+    assert str(obj) == 'masterVariant(prices(country = "NL"))'
+
+
+def test_two_filters():
+    obj = QueryPredicate(
+        masterVariant__slug__nl="some-slug", masterVariant__prices__country="NL"
+    )
+    assert (
+        str(obj)
+        == 'masterVariant(slug(nl = "some-slug")) AND masterVariant(prices(country = "NL"))'
+    )
+
+
+def test_dict_format():
+    obj = QueryPredicate(**{'masterVariant__slug__en-GB': "something"})
+    assert str(obj) == 'masterVariant(slug(en-GB = "something"))'
+
+
+def test_is_defined():
+    obj = QueryPredicate(
+        masterVariant__slug__is_defined=True, masterVariant__prices__is_defined=False
+    )
+    assert (
+        str(obj)
+        == "masterVariant(slug is defined) AND masterVariant(prices is not defined)"
+    )
+
+
+def test_gte():
+    obj = QueryPredicate(
+        masterVariant__slug__nl__exact="some-slug",
+        masterVariant__prices__amount__gte=100,
+    )
+    assert (
+        str(obj)
+        == 'masterVariant(slug(nl = "some-slug")) AND masterVariant(prices(amount >= 100))'
+    )
+
+
+def test_qp_contains_any():
+    obj = QueryPredicate(masterVariant__slug__nl__contains_any=["slug-1", "slug-2"])
+    assert str(obj) == 'masterVariant(slug(nl contains any ("slug-1", "slug-2")))'
+
+
+def test_qp_or():
+    obj = QueryPredicate(
+        masterVariant__slug=QueryPredicate(nl="test-nl") | QueryPredicate(en="test-en")
+    )
+    assert str(obj) == 'masterVariant(slug(nl = "test-nl" OR en = "test-en"))'
+
+
+def test_qp_and():
+    obj = QueryPredicate(
+        masterVariant__slug=QueryPredicate(nl="test-nl") & QueryPredicate(en="test-en")
+    )
+    assert str(obj) == 'masterVariant(slug(nl = "test-nl" AND en = "test-en"))'

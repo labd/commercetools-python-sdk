98996744c207f958216ce6afa6f6dc7c964be66b
Michael van Tellingen <michael@mvantellingen.nl>
2019-01-31 16:49:52 +0100

    Make the api for paginators more user friendly
    The created paginator is now an iterable and also supports the slice
    syntax

--- src/commercetools/paginators.py
+++ src/commercetools/paginators.py
@@ -4,14 +4,21 @@ from commercetools import types
 
 
 class Paginator:
-    """This paginator uses the offset kwarg for retrieving the pages"""
+    """This paginator uses the offset kwarg for retrieving the pages
 
-    def __init__(self, page_size=20):
-        self.page_size = page_size
+    Example::
 
-    def paginate(
-        self, operation: typing.Callable, **kwargs: typing.Dict[str, typing.Any]
-    ) -> typing.Generator[types.Resource, None, None]:
+        paginator = Paginator(client.products.query, sort=["id asc"])
+        for product in paginator:
+            print(product)
+
+        paginator = Paginator(client.products.query, sort=["id asc"])
+        for product in paginator[:-20]:
+            print(product)
+
+    """
+
+    def __init__(self, operation: typing.Callable, **kwargs: typing.Dict[str, typing.Any]):
         if not callable(operation):
             raise ValueError("Expected a callable as first argument")
 
@@ -21,12 +28,44 @@ class Paginator:
                 "keyword arguments when using the paginator."
             )
 
-        offset = 0
+        self.page_size = 20
+        self._operation = operation
+        self._kwargs = kwargs
+        self._offset = 0
+        self._limit = None
+
+    def __iter__(self) -> typing.Generator[types.Resource, None, None]:
+
+        offset = self._offset or 0
+        limit = self._limit
+        num = 0
         while True:
-            response = operation(**kwargs, offset=offset, limit=self.page_size)
+            response = self._operation(**self._kwargs, offset=offset, limit=self.page_size)
+
+            if limit is not None and limit < 0:
+                limit = (response.total + limit) - offset
+
             for item in response.results:
                 yield item
+
+                if limit is not None:
+                    num += 1
+                    if num >= limit:
+                        raise StopIteration()
 
             offset += response.count
             if offset >= response.total:
                 break
+
+    def _clone(self):
+        clone = self.__class__(operation=self._operation, **self._kwargs)
+        clone.page_size = self.page_size
+        return clone
+
+    def __getitem__(self, item):
+        if isinstance(item, slice):
+            clone = self._clone()
+            clone._offset = item.start
+            clone._limit = item.stop
+            return clone
+        raise IndexError

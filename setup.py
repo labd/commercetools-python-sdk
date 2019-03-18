cc3b9cb3d8335ad5e4acb29eb2dd916f8d39bad5
David Weterings <david@davidweterings.nl>
2019-03-18 16:51:39 +0100

    Revert version bump, should have used bumpversion

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ with open("README.rst") as fh:
 
 setup(
     name="commercetools",
-    version="2.4.2",
+    version="2.4.1",
     description="SDK for Commercetools",
     long_description=long_description,
     author="Lab Digital B.V.",

6dc841349892801609cb7682b6a7838321c716a0
David Weterings <david@davidweterings.nl>
2019-04-15 14:04:55 +0200

    Prepare 2.5.1 release

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ with open("README.rst") as fh:
 
 setup(
     name="commercetools",
-    version="2.5.0",
+    version="2.5.1",
     description="SDK for Commercetools",
     long_description=long_description,
     author="Lab Digital B.V.",

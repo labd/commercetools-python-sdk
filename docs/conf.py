d60722f3c37d4980eeba259d9eb12188a6f941b3
David Weterings <david@davidweterings.nl>
2019-04-25 10:44:33 +0200

    Bump version: 2.5.2 → 3.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = "Lab Digital"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "2.5.2"
+release = "3.0.0"
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ with open("README.rst") as fh:
 
 setup(
     name="commercetools",
-    version="2.5.2",
+    version="3.0.0",
     description="SDK for Commercetools",
     long_description=long_description,
     author="Lab Digital B.V.",

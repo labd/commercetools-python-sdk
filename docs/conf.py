5b4d998b6e24c1bcfb4bce7bdfd6a2474aa9457a
David Weterings <david@davidweterings.nl>
2019-05-20 16:59:39 +0200

    Bump version: 3.2.0 → 3.3.0

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = "Lab Digital"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "3.2.0"
+release = "3.3.0"
 
 
 # -- General configuration ---------------------------------------------------

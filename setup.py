8862bd8aa0fcddf38afd63af67a8c70ef39b2d7b
David Weterings <david@davidweterings.nl>
2019-05-23 21:28:25 +0200

    Bump version: 3.4.0 → 3.5.0

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ with open("README.rst") as fh:
 
 setup(
     name="commercetools",
-    version="3.4.0",
+    version="3.5.0",
     description="SDK for Commercetools",
     long_description=long_description,
     author="Lab Digital B.V.",

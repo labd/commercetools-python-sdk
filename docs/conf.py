ce3e4eead07563236b26913f42d1ca70b15d84f2
David Weterings <david@davidweterings.nl>
2019-11-05 13:07:52 +0100

    Bump version: 4.1.0 → 5.0.0

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = "Lab Digital"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "4.1.0"
+release = "5.0.0"
 
 
 # -- General configuration ---------------------------------------------------

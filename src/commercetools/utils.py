85c5c8eb17b0c2f155907d638e39f72b91dc7b83
David Weterings <david@davidweterings.nl>
2019-11-18 17:01:38 +0100

    feat: token_url now also works if /oauth/token is not passed to Client

--- src/commercetools/utils.py
+++ src/commercetools/utils.py
@@ -1,4 +1,5 @@
 import threading
+import urllib.parse
 
 tls = threading.local()
 
@@ -36,3 +37,18 @@ class DefaultTokenSaver(BaseTokenSaver):
     def clear_cache(cls):
         items = getattr(tls, "tokens", {})
         items.clear()
+
+
+def fix_token_url(token_url: str) -> str:
+    """
+    Ensure the token url has the right format.
+
+    Often clients only pass the base url instead of the complete
+    token url, which gets confusing for users.
+    """
+    parts = urllib.parse.urlparse(token_url)
+    if parts.path == "":
+        token_url = urllib.parse.urlunparse(
+            (*parts[:2], "/oauth/token", *parts[3:])
+        )
+    return token_url

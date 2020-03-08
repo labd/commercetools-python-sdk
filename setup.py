8bfe22d78f57889d31cf8bdb56a452662899f520
Michael van Tellingen <michael@mvantellingen.nl>
2020-03-08 14:06:44 +0100

    Bump version: 6.1.0 → 6.2.0

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ with open("README.rst") as fh:
 
 setup(
     name="commercetools",
-    version="6.1.0",
+    version="6.2.0",
     description="SDK for Commercetools",
     long_description=long_description,
     author="Lab Digital B.V.",

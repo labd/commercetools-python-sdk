afb482f9d332ab0ed4d0a539a5de05bcf368e8fb
Tim Leguijt <info@leguijtict.nl>
2020-04-02 09:25:33 +0200

    Check for unique values in testing backend

--- src/commercetools/exceptions.py
+++ src/commercetools/exceptions.py
@@ -1,5 +1,15 @@
+import typing
+
+from commercetools.types import ErrorResponse
+
+
 class CommercetoolsError(Exception):
-    def __init__(self, message, response, correlation_id=None) -> None:
+    response: ErrorResponse
+    correlation_id: typing.Optional[str]
+
+    def __init__(
+        self, message: typing.Any, response: ErrorResponse, correlation_id: str = None
+    ) -> None:
         super().__init__(message)
         self.response = response
         self.correlation_id = correlation_id

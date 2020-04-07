13c39950d1d79c08c471b683b3fbe84d387fe208
Tim Leguijt <info@leguijtict.nl>
2020-04-07 16:20:06 +0200

    Improve CommercetoolsError parsing (#84)
    * Improve CommercetoolsError parsing
    
    It now renders the error details as well for better troubleshooting
    
    * Assure errors is always a list
    
    * Parse error code
    
    * Improved error code parsing

--- src/commercetools/exceptions.py
+++ src/commercetools/exceptions.py
@@ -8,8 +8,46 @@ class CommercetoolsError(Exception):
     correlation_id: typing.Optional[str]
 
     def __init__(
-        self, message: typing.Any, response: ErrorResponse, correlation_id: str = None
+        self,
+        message: typing.Any,
+        errors: typing.List[dict],
+        response: ErrorResponse,
+        correlation_id: str = None,
     ) -> None:
         super().__init__(message)
         self.response = response
+        self.errors = errors
         self.correlation_id = correlation_id
+
+    def __str__(self):
+        result = super().__str__()
+        if self.details:
+            return f"{result} ({', '.join(self.details)})"
+        return result
+
+    @property
+    def details(self) -> typing.List[str]:
+        return [
+            e["detailedErrorMessage"]
+            for e in self.errors
+            if "detailedErrorMessage" in e
+        ]
+
+    @property
+    def codes(self) -> typing.List[str]:
+        try:
+            return [e.code for e in self.response.errors]
+        except AttributeError:
+            return []
+
+    @property
+    def code(self) -> str:
+        """Convenience property to easily get the error code.
+        
+        Returns the code of the first error, just as
+        'message' is always the message of the first error.
+        """
+        try:
+            return self.codes[0]
+        except KeyError:
+            return ""

3f8788c206c61d95450535af58b4a30db17bec17
David Weterings <d.weterings@labdigital.nl>
2020-07-15 12:30:51 +0200

    Bump version: 8.1.4 → 8.1.5

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ with open("README.rst") as fh:
 
 setup(
     name="commercetools",
-    version="8.1.4",
+    version="8.1.5",
     description="SDK for Commercetools",
     long_description=long_description,
     author="Lab Digital B.V.",

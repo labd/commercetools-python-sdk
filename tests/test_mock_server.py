b2a61ef7ea24105189650878409057d993cbb89f
David Weterings <d.weterings@labdigital.nl>
2020-07-21 12:14:01 +0200

    Add unittest to check expanding works in mock server

--- tests/test_mock_server.py
+++ tests/test_mock_server.py
@@ -1,13 +1,13 @@
-import pytest
+import os
+
 import requests
 
 from commercetools import Client
-from commercetools.types import LocalizedString, ProductDraft
+from commercetools.types import LocalizedString, ProductDraft, ChannelDraft, ChannelRoleEnum, ChannelResourceIdentifier, \
+    StoreDraft
 
 
 def test_http_server(commercetools_client, commercetools_http_server):
-    import os
-
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
     client = Client(
@@ -30,3 +30,32 @@ def test_http_server(commercetools_client, commercetools_http_server):
     assert response.status_code == 200, response.text
     data = response.json()
     assert data["masterData"]["staged"]["name"]["nl"] == "Testje"
+
+
+def test_http_server_expanding(commercetools_client, commercetools_http_server):
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    client = Client(
+        project_key="unittest",
+        client_id="client-id",
+        client_secret="client-secret",
+        scope=[],
+        url=commercetools_http_server.api_url,
+        token_url=f"{commercetools_http_server.api_url}/oauth/token",
+    )
+
+    client.channels.create(
+        ChannelDraft(
+            key="FOO", roles=[ChannelRoleEnum.PRODUCT_DISTRIBUTION]
+        )
+    )
+
+    store = client.stores.create(StoreDraft(key="FOO", distribution_channels=[ChannelResourceIdentifier(key="FOO")]))
+
+    url = commercetools_http_server.api_url + f"/unittest/stores/{store.id}"
+    response = requests.get(url, params={"expand": "distributionChannels[*]"}, headers={"Authorization": "Bearer token"})
+
+    assert response.status_code == 200, response.text
+    data = response.json()
+
+    assert data["distributionChannels"][0]["obj"]["key"] == "FOO"

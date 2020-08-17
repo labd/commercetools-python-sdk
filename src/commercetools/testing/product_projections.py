5c83120aec4bb99008612480c984c9aa6f5e84bd
Michael van Tellingen <m.vantellingen@labdigital.nl>
2020-08-17 19:15:39 +0200

    Refactor product projections mock endpoints

--- src/commercetools/testing/product_projections.py
+++ src/commercetools/testing/product_projections.py
@@ -4,6 +4,7 @@ from commercetools import types
 from commercetools._schemas._product import (
     ProductProjectionPagedQueryResponseSchema,
     ProductProjectionSchema,
+    ProductSchema,
 )
 from commercetools.services.product_projections import _ProductProjectionQuerySchema
 from commercetools.testing import utils
@@ -25,106 +26,125 @@ class ProductProjectionsBackend(ServiceBackend):
 
     def query(self, request):
         params = utils.parse_request_params(_ProductProjectionQuerySchema, request)
-        results = [
-            self._convert_product_projection(product, params["staged"])
-            for product in self.model.objects.values()
-        ]
-        results = [x for x in results if x]
-        if params.get("limit"):
-            results = results[: params["limit"]]
+        limit = params.get("limit")
 
-        if params.get("expand"):
-            expanded_results = []
-            for result in results:
-                expanded_results.append(self._expand(request, result))
-            results = expanded_results
+        results = []
+        found = 0
+        for obj in self.model.objects.values():
+            expanded_obj = self._expand(request, obj)
+            product = ProductSchema().load(expanded_obj)
+
+            result = self._convert_product_projection(product, params["staged"])
+            if result:
+                results.append(result)
+                found += 1
+
+            if limit is not None and found == limit:
+                break
 
         data = {
             "count": len(results),
             "total": len(results),
             "offset": 0,
             "results": results,
+            "limit": limit,
         }
         content = ProductProjectionPagedQueryResponseSchema().dumps(data)
         return create_commercetools_response(request, text=content)
 
     def search(self, request):
-        params = utils.parse_request_params(ProductProjectionsQuerySchema, request)
-        results = [
-            self._convert_product_projection(product, params["staged"])
-            for product in self.model.objects.values()
-        ]
-        results = [x for x in results if x]
-        if params.get("limit"):
-            results = results[: params["limit"]]
+        params = utils.parse_request_params(_ProductProjectionQuerySchema, request)
 
-        if params.get("expand"):
-            expanded_results = []
-            for result in results:
-                expanded_results.append(self._expand(request, result))
-            results = expanded_results
+        limit = params.get("limit")
+
+        results = []
+        found = 0
+        for obj in self.model.objects.values():
+            expanded_obj = self._expand(request, obj)
+            product = ProductSchema().load(expanded_obj)
+
+            result = self._convert_product_projection(product, params["staged"])
+            if result:
+                results.append(results)
+                found += 1
+
+            if limit is not None and found == limit:
+                break
 
         data = {
             "count": len(results),
-            "total": len(results),
+            "total": len(results),  # FIXME
             "offset": 0,
             "results": results,
+            "limit": limit,
         }
         content = ProductProjectionPagedQueryResponseSchema().dumps(data)
         return create_commercetools_response(request, text=content)
 
     def get_by_id(self, request, id):
+        params = utils.parse_request_params(_ProductProjectionQuerySchema, request)
         obj = self.model.get_by_id(id)
         if obj:
             expanded_obj = self._expand(request, obj)
-            content = ProductProjectionSchema().dumps(expanded_obj)
+            product = ProductSchema().load(expanded_obj)
+
+            result = self._convert_product_projection(product, params["staged"])
+            if not result:
+                return create_commercetools_response(request, status_code=404)
+            content = ProductProjectionSchema().dumps(result)
             return create_commercetools_response(request, text=content)
         return create_commercetools_response(request, status_code=404)
 
     def get_by_key(self, request, key):
+        params = utils.parse_request_params(_ProductProjectionQuerySchema, request)
         obj = self.model.get_by_key(key)
         if obj:
             expanded_obj = self._expand(request, obj)
-            content = ProductProjectionSchema().dumps(expanded_obj)
+            product = ProductSchema().load(expanded_obj)
+
+            result = self._convert_product_projection(product, params["staged"])
+            if not result:
+                return create_commercetools_response(request, status_code=404)
+            content = ProductProjectionSchema().dumps(result)
             return create_commercetools_response(request, text=content)
         return create_commercetools_response(request, status_code=404)
 
     def _convert_product_projection(
-        self, product: typing.Dict, staged: bool = False
+        self, product: types.Product, staged: bool = False
     ) -> typing.Optional[typing.Dict]:
         """Convert a Product object to a ProductProjection object"""
-        if product["masterData"] is None:
+        if product.master_data is None:
             return None
 
         if staged:
-            data = product["masterData"]["staged"]
+            data: types.ProductData = product.master_data.staged
         else:
-            data = product["masterData"]["current"]
+            data: types.ProductData = product.master_data.current
 
         if data is None:
             return None
 
-        return {
-            "id": product["id"],
-            "key": product["key"],
-            "version": product["version"],
-            "createdAt": product["createdAt"],
-            "lastModifiedAt": product["lastModifiedAt"],
-            "productType": product["productType"],
-            "name": data["name"],
-            "description": data["description"],
-            "slug": data["slug"],
-            "categories": data["categories"],
-            "categoryOrderHints": data["categoryOrderHints"],
-            "metaTitle": data["metaTitle"],
-            "metaDescription": data["metaDescription"],
-            "metaKeywords": data["metaKeywords"],
-            "searchKeywords": data["searchKeywords"],
-            "hasStagedChanges": product["masterData"]["hasStagedChanges"],
-            "published": product["masterData"]["published"],
-            "masterVariant": data["masterVariant"],
-            "variants": data["variants"],
-            "taxCategory": product["taxCategory"],
-            "state": product["state"],
-            "reviewRatingStatistics": product["reviewRatingStatistics"],
-        }
+        return types.ProductProjection(
+            id=product.id,
+            version=product.version,
+            key=product.key,
+            created_at=product.created_at,
+            last_modified_at=product.last_modified_at,
+            product_type=product.product_type,
+            name=data.name,
+            description=data.description,
+            slug=data.slug,
+            categories=data.categories,
+            category_order_hints=data.category_order_hints,
+            meta_title=data.meta_title,
+            meta_description=data.meta_description,
+            meta_keywords=data.meta_keywords,
+            search_keywords=data.search_keywords,
+            has_staged_changes=product.master_data.has_staged_changes,
+            published=product.master_data.published,
+            master_variant=data.master_variant,
+            variants=data.variants,
+            tax_category=product.tax_category,
+            state=product.state,
+            review_rating_statistics=product.review_rating_statistics,
+        )

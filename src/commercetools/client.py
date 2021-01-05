9e6cbda3dbfb9d3939f4aec7962e211c00697acc
Michael van Tellingen <m.vantellingen@labdigital.nl>
2021-01-05 18:45:13 +0100

    Update client

--- src/commercetools/client.py
+++ src/commercetools/client.py
@@ -113,27 +113,23 @@ class BaseClient:
         self,
         endpoint: str,
         params: typing.Dict[str, typing.Any],
-        schema_cls: typing.Type[SchemaABC] = None,
-        response_object: typing.Type[SchemaABC] = None,
-        headers=None,
+        response_class: typing.Type[SchemaABC] = None,
+        headers: typing.Dict[str, str] = None,
     ) -> typing.Any:
         """Retrieve a single object from the commercetools platform"""
         response = self._http_client.get(self._base_url + endpoint, params=params)
 
         if response.status_code == 200:
-            if response_object:
-                return response_object.deserialize(response.json())
-            return schema_cls().load(response.json())
+            return response_class.deserialize(response.json())
         return self._process_error(response)
 
     def _post(
         self,
         endpoint: str,
-        params: typing.Dict[str, str],
-        data_object: typing.Any,
-        request_schema_cls: typing.Optional[typing.Type[SchemaABC]],
-        response_schema_cls: typing.Type[SchemaABC],
-        response_object: typing.Type[SchemaABC] = None,
+        params: typing.Dict[str, typing.Any],
+        data_object: typing.Any = None,
+        response_class: typing.Type[SchemaABC] = None,
+        headers: typing.Dict[str, str] = None,
         form_encoded: bool = False,
         force_update: bool = False,
     ) -> typing.Any:
@@ -150,9 +146,8 @@ class BaseClient:
 
             response = self._http_client.post(self._base_url + endpoint, **kwargs)
             if response.status_code in (200, 201):
-                if response_object:
-                    return response_object.deserialize(response.json())
-                return response_schema_cls().load(response.json())
+                if response_class:
+                    return response_class.deserialize(response.json())
             return self._process_error(response)
 
         if data_object is not None:
@@ -164,10 +159,9 @@ class BaseClient:
     def _upload(
         self,
         endpoint: str,
-        params: typing.Dict[str, str],
+        params: typing.Dict[str, typing.Any],
         file: typing.IO,
-        response_schema_cls: typing.Type[SchemaABC],
-        response_object: typing.Type[SchemaABC] = None,
+        response_class: typing.Type[SchemaABC] = None,
     ) -> typing.Any:
         """Retrieve a single object from the commercetools platform"""
         response = self._http_client.post(
@@ -175,17 +169,15 @@ class BaseClient:
         )
 
         if response.status_code in (200, 201):
-            if response_object:
-                return response_object.deserialize(response.json())
-            return response_schema_cls().load(response.json())
+            return response_class.deserialize(response.json())
         return self._process_error(response)
 
     def _delete(
         self,
         endpoint: str,
-        params: typing.Dict[str, str],
-        response_schema_cls: typing.Type[SchemaABC],
-        response_object: typing.Type[SchemaABC] = None,
+        params: typing.Dict[str, typing.Any],
+        response_class: typing.Type[SchemaABC] = None,
+        headers: typing.Dict[str, str] = None,
         force_delete: bool = False,
     ) -> typing.Any:
         """Delete an object from the commercetools platform"""
@@ -196,9 +188,8 @@ class BaseClient:
                 self._base_url + endpoint, params=params
             )
             if response.status_code == 200:
-                if response_object:
-                    return response_object.deserialize(response.json())
-                return response_schema_cls().load(response.json())
+                if response_class:
+                    return response_class.deserialize(response.json())
             return self._process_error(response)
 
         return remote_http_call(params)
@@ -254,7 +245,6 @@ class BaseClient:
 
 
 class Client(BaseClient, ServicesMixin):
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self._base_url = f"{self._config['url']}/{self._config['project_key']}/"

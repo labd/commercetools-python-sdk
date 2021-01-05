599c21b0585dc45a96410fe92d683399df1b7ba4
Michael van Tellingen <m.vantellingen@labdigital.nl>
2021-01-05 21:11:53 +0100

    Update client

--- src/commercetools/protocols.py
+++ src/commercetools/protocols.py
@@ -3,41 +3,10 @@ import typing
 from marshmallow.base import SchemaABC
 
 
-class ClientProtocol(typing.Protocol):
-    def _get(
-        self,
-        endpoint: str,
-        params: typing.Dict[str, typing.Any],
-        schema_cls: typing.Type[SchemaABC],
-    ) -> typing.Any:
+class Model(typing.Protocol[T]):
+    @classmethod
+    def deserialize(cls, data: typing.Dict[str, typing.Any]) -> T:
         ...
 
-    def _post(
-        self,
-        endpoint: str,
-        params: typing.Dict[str, str],
-        data_object: typing.Any,
-        request_schema_cls: typing.Optional[typing.Type[SchemaABC]],
-        response_schema_cls: typing.Type[SchemaABC],
-        form_encoded: bool = False,
-        force_update: bool = False,
-    ) -> typing.Any:
-        ...
-
-    def _upload(
-        self,
-        endpoint: str,
-        params: typing.Dict[str, str],
-        file: typing.IO,
-        response_schema_cls: typing.Type[SchemaABC],
-    ) -> typing.Any:
-        ...
-
-    def _delete(
-        self,
-        endpoint: str,
-        params: typing.Dict[str, str],
-        response_schema_cls: typing.Type[SchemaABC],
-        force_delete: bool = False,
-    ) -> typing.Any:
+    def serialize(self) -> typing.Dict[str, typing.Any]:
         ...

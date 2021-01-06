7a55533ef48904f4671348b16b0ca8342ef1c51a
Michael van Tellingen <m.vantellingen@labdigital.nl>
2021-01-06 18:39:26 +0100

    Add a BaseSchema resource
    This adds a post_dump hook to remove `None` data if the field is marked
    as omit_empty

---
+++ tests/importapi/test_models.py
@@ -0,0 +1,32 @@
+import pprint
+from commercetools.importapi import models
+
+
+def test_serialization_optional_values():
+    item = models.LineItemImportDraft(
+        name={"nl-NL": "foobar"},
+        variant=models.LineItemProductVariantImportDraft(sku="foobar"),
+        price=models.LineItemPrice(
+            value=models.Money(cent_amount=1000, currency_code="EUR"),
+        ),
+        quantity=3,
+    )
+
+    data = item.serialize()
+    item_cycle = models.LineItemImportDraft.deserialize(data)
+    data_cycle = item_cycle.serialize()
+
+    expected = {
+        "name": {"nl-NL": "foobar"},
+        "price": {
+            "value": {
+                "centAmount": 1000,
+                "currencyCode": "EUR",
+                "type": "centPrecision",
+            }
+        },
+        "quantity": 3.0,
+        "variant": {"sku": "foobar"},
+    }
+    assert expected == data
+    assert expected == data_cycle

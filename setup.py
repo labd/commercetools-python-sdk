b18a1a25b994a19d75ab6e9797edc3652cdc03a5
Michael van Tellingen <m.vantellingen@labdigital.nl>
2021-02-09 12:12:23 +0100

    Bump version: 13.0.0 → 14.0.0b1

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ with open("README.rst") as fh:
 
 setup(
     name="commercetools",
-    version="13.0.0",
+    version="14.0.0b1",
     description="SDK for Commercetools",
     long_description=long_description,
     author="Lab Digital B.V.",

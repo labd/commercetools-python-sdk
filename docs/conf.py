8a2131c3aa9f06734f0b148015357be8b84ec118
Tim Leguijt <t.leguijt@labdigital.nl>
2021-09-15 21:50:46 +0200

    Bump version: 14.0.0b10 → 14.0.0b11

--- docs/conf.py
+++ docs/conf.py
@@ -26,7 +26,7 @@ author = "Lab Digital"
 # The short X.Y version
 version = ""
 # The full version, including alpha/beta/rc tags
-release = "14.0.0b10"
+release = "14.0.0b11"
 
 
 # -- General configuration ---------------------------------------------------
